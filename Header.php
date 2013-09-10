45e272344933a5fa4e0762b66c9e2f9d5a69a2c7
Janeane Foster <janeane.foster@techsupportbase.net>
2013-09-10 15:23:01 +0100

    uploaded

---
+++ Header.php
@@ -0,0 +1,40 @@
+<!doctype html>   
+<head>   
+<meta charset="utf-8">
+ 
+ 
+<title>Security Services North West</title>
+
+
+<meta name="Security services" content="suppling the best for your needs"> 
+  
+<meta name="TechnologyChick" content="SSNWLTD">
+
+<!-- Header -->
+			<div id="header-wrapper">
+				<header class="container" id="site-header">
+					<div class="row">
+						<div class="12u">
+							<div id="logo">
+								<h1>SSNW LTD</h1>
+							</div>
+							<nav id="nav">
+								<ul>
+									<li class="current_page_item"><a href="index.html">Homepage</a></li>
+									<li><a href="threecolumn1.html">Three Column #1</a></li>
+									<li><a href="threecolumn2.html">Three Column #2</a></li>
+									<li><a href="twocolumn1.html">Two Column #1</a></li>
+									<li><a href="twocolumn2.html">Two Column #2</a></li>
+									<li><a href="onecolumn.html">One Column</a></li>
+									<li><a href="contact.html"> Contact Us</a></li>
+								</ul>
+							</nav>
+						</div>
+					</div>
+				</header>
+			</div>
+
+</head>
+
+<body>
+		
